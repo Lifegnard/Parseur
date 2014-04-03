6491928811244d87940caaa746509a94ed757737
GuzzettaLoic <loic.guzzetta@live.fr>
2014-04-03 15:08:28 +0200

    Refactoring générale
    Re conception, agencement du code.
    Suppression des dépendances devenues obselètes

---
+++ parseur/src/main/java/parseurImpl/Parser.java
@@ -0,0 +1,25 @@
+package parseurImpl;
+
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+	QuizReaderImpl r = new QuizReaderImpl();
+	public void parse(Reader reader) throws Exception {
+		Question nouvelleQuestion = new Question();
+		List<Reponse> propositionsDeReponses = new ArrayList<Reponse>();
+		Reponse reponseTmp = null;
+		try {
+			nouvelleQuestion.setIntituleQuestion(r.lireIntitule(reader));
+			r.lireTypeQuestion(reader);
+			while ((reponseTmp = r.lireReponse(reader)) != null) {
+				propositionsDeReponses.add(reponseTmp);
+			}
+			nouvelleQuestion.setPropositionReponses(propositionsDeReponses);
+		} catch (Exception e) {
+			e.toString();
+		}
+
+	}
+}
---
+++ parseur/src/main/java/parseurImpl/QuizReader.java
@@ -0,0 +1,9 @@
+package parseurImpl;
+
+import java.io.Reader;
+
+public interface QuizReader {
+	public String lireIntitule(Reader flux) throws Exception;
+	public String lireTypeQuestion(Reader flux) throws Exception;
+	public Reponse lireReponse(Reader flux) throws Exception;
+}
