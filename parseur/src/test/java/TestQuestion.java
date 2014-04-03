848348436cc4700c9f425f25bb839c7d1fcdbcb2
Evans <Evans@172.19.16.183>
2014-04-03 15:10:57 +0200

    Push du test question refactor

--- parseur/src/test/java/TestQuestion.java
+++ parseur/src/test/java/TestQuestion.java
@@ -10,46 +10,65 @@ import parseurImpl.Reponse;
 
 public class TestQuestion {
 
+	ArrayList<Reponse> listeReponse;
+	Question questionTest;
+	ArrayList<Reponse> nouvelleListeReponse;
+	Question questionTest2;
+	
+	public void setUp() throws Exception {
+		listeReponse=new ArrayList<Reponse>();
+		questionTest=new Question("Intitule de la question de test",listeReponse);	
+		nouvelleListeReponse=new ArrayList<Reponse>();
+		questionTest2=new Question();
+	}
+	
+	public void tearDown() throws Exception {
+		listeReponse = null;
+		questionTest = null;
+		nouvelleListeReponse = null;
+		questionTest2 = null;
+	}
+	
 	@Test
 	public void testGetIntitule() {
-		ArrayList<Reponse> listeReponse=new ArrayList<Reponse>();
-		Question questionTest=new Question("Intitule de la question de test",listeReponse);
+		//ArrayList<Reponse> listeReponse=new ArrayList<Reponse>();
+		//Question questionTest=new Question("Intitule de la question de test",listeReponse);
 		assertEquals("Intitule de la question de test",questionTest.getIntituleQuestion());
 	}
 	
 	@Test
 	public void testSetIntitule() {
-		ArrayList<Reponse> listeReponse=new ArrayList<Reponse>();
-		Question questionTest=new Question("Intitule de la question de test",listeReponse);
+		//ArrayList<Reponse> listeReponse=new ArrayList<Reponse>();
+		//Question questionTest=new Question("Intitule de la question de test",listeReponse);
 		questionTest.setIntituleQuestion("Nouvel intitule");
 		assertEquals("Nouvel intitule",questionTest.getIntituleQuestion());
 	}
 	
 	@Test
 	public void testgetPropositionReponses() {
-		ArrayList<Reponse> listeReponse=new ArrayList<Reponse>();
+		//ArrayList<Reponse> listeReponse=new ArrayList<Reponse>();
 		listeReponse.add(new Reponse("reponse 1 test",true));
-		Question questionTest=new Question("Intitule de la question de test",listeReponse);
+		//Question questionTest=new Question("Intitule de la question de test",listeReponse);
 		assertEquals(listeReponse,questionTest.getPropositionReponses());
 	}
 	
 	@Test
 	public void testsetPropositionReponses() {
-		ArrayList<Reponse> listeReponse=new ArrayList<Reponse>();
+		//ArrayList<Reponse> listeReponse=new ArrayList<Reponse>();
 		listeReponse.add(new Reponse("reponse 1 test",true));
-		ArrayList<Reponse> nouvelleListeReponse=new ArrayList<Reponse>();
+		//ArrayList<Reponse> nouvelleListeReponse=new ArrayList<Reponse>();
 		nouvelleListeReponse.add(new Reponse("reponse 2 test",false));
 		
-		Question questionTest=new Question("Intitule de la question de test",listeReponse);
+		//Question questionTest=new Question("Intitule de la question de test",listeReponse);
 		questionTest.setPropositionReponses(nouvelleListeReponse);
 		assertEquals(nouvelleListeReponse,questionTest.getPropositionReponses());
 	}
 	
 	@Test
 	public void testConstructeurAvecParametre() {
-		ArrayList<Reponse> listeReponse=new ArrayList<Reponse>();
+		//ArrayList<Reponse> listeReponse=new ArrayList<Reponse>();
 		listeReponse.add(new Reponse("reponse 1 test",true));
-		Question questionTest=new Question("Intitule de la question de test",listeReponse);
+		//Question questionTest=new Question("Intitule de la question de test",listeReponse);
 		
 		assertEquals(listeReponse,questionTest.getPropositionReponses());
 		assertEquals("Intitule de la question de test",questionTest.getIntituleQuestion());
@@ -57,10 +76,10 @@ public class TestQuestion {
 	
 	@Test
 	public void testConstructeurSansParametre() {
-		Question questionTest=new Question();
+		//Question questionTest2=new Question();
 		
-		assertEquals("",questionTest.getIntituleQuestion());
-		assertEquals(null,questionTest.getPropositionReponses());
+		assertEquals("",questionTest2.getIntituleQuestion());
+		assertEquals(null,questionTest2.getPropositionReponses());
 	}
 
 }
