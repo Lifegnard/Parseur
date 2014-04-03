4084fdfb384a60c3d925aee30f90a23a5b1db955
GuzzettaLoic <loic.guzzetta@live.fr>
2014-04-03 15:38:45 +0200

    Refactoring classe TestQuestion - code plus propre
    Ajout d'un set up qui regroupe toutes les instanciations.

--- parseur/src/test/java/TestQuestion.java
+++ parseur/src/test/java/TestQuestion.java
@@ -1,69 +1,72 @@
 import static org.junit.Assert.*;
+
 import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+
 import parseurImpl.Question;
 import parseurImpl.Reponse;
 
 public class TestQuestion {
 
+	private List<Reponse> listReponse;
+	private List<Reponse> nouvelleListeReponse;
+	private Question questionTest;
+
+	public void setup() {
+		this.listReponse = new ArrayList<Reponse>();
+		this.nouvelleListeReponse = new ArrayList<Reponse>();
+	}
+
 	@Test
 	public void testGetIntitule() {
-		ArrayList<Reponse> listeReponse = new ArrayList<Reponse>();
-		Question questionTest = new Question("Intitule de la question de test",
-				listeReponse);
-		assertEquals("Intitule de la question de test",
-				questionTest.getIntituleQuestion());
+		this.questionTest = new Question("Intitule de la question de test", this.listReponse);
+		
+		assertEquals("Intitule de la question de test", this.questionTest.getIntituleQuestion());
 	}
 
 	@Test
 	public void testSetIntitule() {
-		ArrayList<Reponse> listeReponse = new ArrayList<Reponse>();
-		Question questionTest = new Question("Intitule de la question de test",
-				listeReponse);
-		questionTest.setIntituleQuestion("Nouvel intitule");
-		assertEquals("Nouvel intitule", questionTest.getIntituleQuestion());
+		this.questionTest = new Question("Intitule de la question de test", this.listReponse);
+		this.questionTest.setIntituleQuestion("Nouvel intitule");
+		
+		assertEquals("Nouvel intitule", this.questionTest.getIntituleQuestion());
 	}
 
 	@Test
 	public void testgetPropositionReponses() {
-		ArrayList<Reponse> listeReponse = new ArrayList<Reponse>();
-		listeReponse.add(new Reponse("reponse 1 test", true));
-		Question questionTest = new Question("Intitule de la question de test",
-				listeReponse);
-		assertEquals(listeReponse, questionTest.getPropositionReponses());
+		this.listReponse.add(new Reponse("reponse 1 test", true));
+		this.questionTest = new Question("Intitule de la question de test", this.listReponse);
+		
+		assertEquals(this.listReponse, this.questionTest.getPropositionReponses());
 	}
 
 	@Test
 	public void testsetPropositionReponses() {
-		ArrayList<Reponse> listeReponse = new ArrayList<Reponse>();
-		listeReponse.add(new Reponse("reponse 1 test", true));
-		ArrayList<Reponse> nouvelleListeReponse = new ArrayList<Reponse>();
-		nouvelleListeReponse.add(new Reponse("reponse 2 test", false));
-
-		Question questionTest = new Question("Intitule de la question de test",
-				listeReponse);
-		questionTest.setPropositionReponses(nouvelleListeReponse);
-		assertEquals(nouvelleListeReponse,
-				questionTest.getPropositionReponses());
+		this.listReponse.add(new Reponse("reponse 1 test", true));
+		this.nouvelleListeReponse.add(new Reponse("reponse 2 test", false));
+		this.questionTest = new Question("Intitule de la question de test", this.listReponse);
+		this.questionTest.setPropositionReponses(this.nouvelleListeReponse);
+		
+		assertEquals(this.nouvelleListeReponse, this.questionTest.getPropositionReponses());
 	}
 
 	@Test
 	public void testConstructeurAvecParametre() {
-		ArrayList<Reponse> listeReponse = new ArrayList<Reponse>();
-		listeReponse.add(new Reponse("reponse 1 test", true));
-		Question questionTest = new Question("Intitule de la question de test",
-				listeReponse);
+		this.listReponse.add(new Reponse("reponse 1 test", true));
+		this.questionTest = new Question("Intitule de la question de test", this.listReponse);
 
-		assertEquals(listeReponse, questionTest.getPropositionReponses());
-		assertEquals("Intitule de la question de test",
-				questionTest.getIntituleQuestion());
+		assertEquals(this.listReponse, this.questionTest.getPropositionReponses());
+		assertEquals("Intitule de la question de test", this.questionTest.getIntituleQuestion());
 	}
 
 	@Test
 	public void testConstructeurSansParametre() {
-		Question questionTest = new Question();
-		assertEquals("", questionTest.getIntituleQuestion());
-		assertEquals(null, questionTest.getPropositionReponses());
+		this.questionTest = new Question();
+		
+		assertEquals("", this.questionTest.getIntituleQuestion());
+		assertEquals(null, this.questionTest.getPropositionReponses());
 	}
 
 }
