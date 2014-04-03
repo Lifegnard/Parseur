ec50376a0ed54dc0a21c0acbb5c844ca6bd13e4c
Romain <Romain@Romain-PC>
2014-04-03 19:14:40 +0200

    Refactoring classe Question

--- parseur/src/main/java/parseurImpl/Question.java
+++ parseur/src/main/java/parseurImpl/Question.java
@@ -5,6 +5,16 @@ import java.util.List;
 public class Question {
 	private String intituleQuestion;
 	private List<Reponse> propositionReponses;
+	
+	public Question(String intituleQuestion, List<Reponse> propositionReponses) {
+		this.intituleQuestion = intituleQuestion;
+		this.propositionReponses = propositionReponses;
+	}
+
+	public Question() {
+		this.intituleQuestion = "";
+		this.propositionReponses = null;
+	}
 
 	public String getIntituleQuestion() {
 		return intituleQuestion;
@@ -22,18 +32,8 @@ public class Question {
 		this.propositionReponses = propositionReponses;
 	}
 
-	public Question(String intituleQuestion, List<Reponse> propositionReponses) {
-		this.intituleQuestion = intituleQuestion;
-		this.propositionReponses = propositionReponses;
-	}
-
-	public Question() {
-		this.intituleQuestion = "";
-		this.propositionReponses = null;
-	}
-
 	public String toString() {
-		return "Intitulé de la question : " + this.intituleQuestion
+		return "Intitulï¿½ de la question : " + this.intituleQuestion
 				+ "\nPropositions de reponses :\n" + propositionReponses;
 	}
 
