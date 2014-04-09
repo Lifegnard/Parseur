2f522993785abe0e8dd4345e738c4105ec874f3e
Romain <Romain@Romain-PC>
2014-04-09 19:36:36 +0200

    - Ajout de commentaire - Ajout d'un test de la fonction lireReponse

--- parseur/src/test/java/TestQuizReaderImpl.java
+++ parseur/src/test/java/TestQuizReaderImpl.java
@@ -21,7 +21,8 @@ public class TestQuizReaderImpl {
 		pReader = new QuizReaderImpl();
 	}
 	
-	
+	// Test lorsqu'on rédige un intitulé non conforme aux règles
+	// Déclenche en conséquence une exception
 	@Test
 	public void testExceptionIntituleReaderNonConforme() throws Exception{
 		boolean error = false;
@@ -35,6 +36,8 @@ public class TestQuizReaderImpl {
 		  assertTrue(error);
 	}
 	
+	// Test lorsqu'on rédige un intitulé sans l'accolade d'ouverte
+	// Doit déclencher l'exception
 	@Test
 	public void testExceptionIntituleReaderNonConformeAbsenceAccolade() throws Exception{
 		boolean error = false;
@@ -48,6 +51,8 @@ public class TestQuizReaderImpl {
 		  assertTrue(error);
 	}
 	
+	// Test lorsqu'on rédige un intitulé sans le Pipe fermant
+	// Doit déclencher l'exception
 	@Test
 	public void testExceptionIntituleReaderNonConformeAbsencePipe() throws Exception{
 		boolean error = false;
@@ -61,6 +66,8 @@ public class TestQuizReaderImpl {
 		  assertTrue(error);
 	}
 	
+	// Test lorsqu'on rédige un intitulé vide
+	// Doit déclencher l'exception
 	@Test
 	public void testExceptionIntituleReaderVide() throws Exception{
 		boolean error = false;
@@ -74,7 +81,8 @@ public class TestQuizReaderImpl {
 		  assertTrue(error);
 	}
 	
-	
+	// Test lorsqu'on rédige un intitulé correcte
+	// Doit renvoyer une chaine de caractère correspondant à l'intitulé
 	@Test
 	public void testExceptionIntituleReaderValide() throws Exception{
 		boolean error = false;
@@ -90,6 +98,8 @@ public class TestQuizReaderImpl {
 	}
 	
 
+	// Test lorsqu'on rédige un type de question non conforme
+	// Doit déclencher l'exception
 	@Test
 	public void testExceptionTypeQuestionReaderNonConforme() throws Exception{
 		boolean error = false;
@@ -103,6 +113,8 @@ public class TestQuizReaderImpl {
 		  assertTrue(error);
 	}
 	
+	// Test lorsqu'on rédige un type de question sans guillemets ouvrante
+	// Doit déclencher l'exception
 	@Test
 	public void testExceptionTypeQuestionReaderNonConformeAbsenceFirstGuillemet() throws Exception{
 		boolean error = false;
@@ -116,6 +128,8 @@ public class TestQuizReaderImpl {
 		  assertTrue(error);
 	}
 	
+	// Test lorsqu'on rédige un type de question sans guillemets fermantes
+	// Doit déclencher l'exception
 	@Test
 	public void testExceptionTypeQuestionReaderNonConformeAbsenceSecondGuillemet() throws Exception{
 		boolean error = false;
@@ -129,6 +143,8 @@ public class TestQuizReaderImpl {
 		  assertTrue(error);
 	}
 	
+	// Test lorsqu'on rédige un type de question vide
+	// Doit déclencher l'exception
 	@Test
 	public void testExceptionTypeQuestionReaderVide() throws Exception{
 		boolean error = false;
@@ -143,6 +159,8 @@ public class TestQuizReaderImpl {
 	}
 	
 	
+	// Test lorsqu'on rédige un type de question valide
+	// Doit renvoyer la chaine de caractère correspondant aux type de question
 	@Test
 	public void testExceptionTypeQuestionReaderValide() throws Exception{
 		boolean error = false;
@@ -157,7 +175,8 @@ public class TestQuizReaderImpl {
 		  assertEquals("()",result);
 	}
 	
-	
+	// Test lorsqu'on rédige une reponse fausse
+	// Doit renvoyer une Reponse non correct 
 	@Test
 	public void testReponseFausseReaderValide() throws Exception{
 		boolean error = false;
@@ -173,6 +192,8 @@ public class TestQuizReaderImpl {
 		  assertFalse(rep.isCorrect());
 	}
 	
+	// Test lorsqu'on rédige une reponse valide
+	// Doit renvoyer une Reponse  correct 
 	@Test
 	public void testReponseVraiReaderValide() throws Exception{
 		boolean error = false;
@@ -188,6 +209,8 @@ public class TestQuizReaderImpl {
 		  assertTrue(rep.isCorrect());
 	}
 	
+	// Test lorsqu'on rédige une reponse mal rédigé, avec une absence de point
+	// Doit renvoyer null
 	@Test
 	public void testReponseReaderAbsencePoint() throws Exception{
 		boolean error = false;
@@ -202,6 +225,8 @@ public class TestQuizReaderImpl {
 		  assertEquals(null,rep);
 	}
 	
+	// Test lorsqu'on rédige une reponse mal rédigé, avec une absence de signe
+	// Doit renvoyer null
 	@Test
 	public void testReponseReaderAbsenceSigne() throws Exception{
 		boolean error = false;
@@ -216,6 +241,24 @@ public class TestQuizReaderImpl {
 		  assertEquals(null,rep);
 	}
 	
+	// Test lorsqu'on rédige une reponse avec une chaine vide
+	// Doit renvoyer null
+	@Test
+	public void testReponseReaderChaineVide() throws Exception{
+		boolean error = false;
+		Reader reader = new StringReader("");
+		Reponse rep = new Reponse();
+		  try {
+			  rep = pReader.lireReponse(reader);
+		  } catch (Exception ex) {	
+			  error = true;
+		  }
+		  assertFalse(error);
+		  assertEquals(null,rep);
+	}
+	
+	// Test lorsqu'on rédige une reponse avec un saut de ligne
+	// Doit renvoyer une réponse correcte
 	@Test
 	public void testReponseReaderSautDeLigne() throws Exception{
 		boolean error = false;
